04be829fe9bb94c3451ce820d3e685a867258ea7
obatbayar <oyuna.alisa@gmail.com>
2017-05-16 19:06:30 -0700

    Zodiac App

---
+++ js/main.js
@@ -0,0 +1,113 @@
+var horoscope = [
+	{
+		name: "aries",
+		date: "March 21 - April 19",
+		image: "img/aries.png",
+		text: "Aries people are creative, adaptive, and insightful. They can also be strong-willed and spontaneous (sometimes to a fault). Aries people can be driven and are very ambitious often making them over-achievers in anything they set their mind to tackle. Aries are fire signs, and so too is their personality. They may be quick to anger, but don't take it personally, it's just their fiery, passionate personalities showing through. Aries signs have excellent sense of humor, and they get along with almost everyone at the party (and they DO know how to party). Aries can be impatient, but we love them anyway because they are devoted friends, lovers and family members - they are loyal to then end and will fight for their causes (usually supporting the underdog)."
+	},
+	{
+		name: "taurus",
+		date: "April 20 - May 20",
+		image: "img/taurus.png",
+		text: "Taurus zodiac signs and meanings, like the animal that represents them, is all about strength, stamina and will. Stubborn by nature, the Taurus will stand his/her ground to the bitter end (sometimes even irrationally so). But that's okay because the Taurus is also a loving, sympathetic and appreciative sign. The Taurus is very understanding and when we need someone to unburden ourselves to, we often share our deepest fears with the Taurians of the zodiac. Taurians are very patient, practical and efficient, they are excellent in matters of business and are also wonderful instructors/teachers. Although initially they may have their own best interest at heart, they are ultimately and endlessly generous with their time, possessions and love."
+	},
+	{
+		name: "gemini",
+		date: "May 21 - June 20",
+		image: "img/gemini.png",
+		text: "Flexibility, balance and adaptability are the keywords for the Gemini. They are quick to grasp the meaning of a situation and act on it, often with positive effects. They tend to have a duality to their nature, and can sometimes be tough to predict how they will react. They can turn from hot to cold and may be prone to noticeable mood swings. However, they are generous signs with tendencies of being affectionate, and imaginative. They also inspire others easily as they seem to naturally motivate themselves - their charisma and accomplishments are infectious. Geminians are very supportive, and are especially good at promotions, sales, and driving hard bargains."
+	},
+	{
+		name: "cancer",
+		date: "June 21 - July 22",
+		image: "img/cancer.png",
+		text: "Cancerians love home-life, family and domestic settings. They are traditionalists, and enjoy operating on a fundamental level. They love history, and are fascinated with the beginnings of things (heraldry, ancestry, etc.). The moon is their ruler, so they can be a bit of a contradiction and sometimes moody. However, they are conservative, so they'll be apt to hide their moods from others altogether. They have a reputation for being fickle, but they'll tell you that isn't true, and it's not. Cancerians make loyal, sympathetic friends. However Cancerians need alone time, and when they retreat, let them do so on their terms."
+	},
+	{
+		name: "leo",
+		date: "July 23 - August 22",
+		image: "img/leo.png",
+		text: "The zodiac signs and meanings of Leo is about expanse, power and exuberance. Leo's are natural born leaders, and they will let you know it as they have a tendency to be high-minded and vocal about their opinions. That's okay, because if you observe, the Leo is usually correct in his/her statements. Leo's have a savvy way of analyzing a situation and executing swift judgment with a beneficial outcome. It comes from being a leader. They are brave, intuitive, and also head-strong and willful. Beneath their dynamic persona lies a generous, loving, sensitive nature that they do not easily share with others. They might be a bit bossy, but those who know them understand this comes from a source need to do good, not (usually) from an inflated ego."
+	},
+	{
+		name: "virgo",
+		date: "August 23 - September 22",
+		image: "img/virgo.png",
+		text: "Virgo's have keen minds, and are delightful to chat with, often convincing others of outlandish tales with ease and charm. Virgo's are inquisitive and are very skilled at drawing information from people. This trait also makes them naturally intuitive. Combine this with their remarkable memories, and we see an advanced, analytical personality. However the Virgo needs balance in their lives otherwise they may become short-tempered, impatient and self-serving. Virgo's are excellent teammates in work and social activities. They work well with others, although they freely express their opinions (even when unwarranted)."
+	},
+	{
+		name: "libra",
+		date: "September 23 - October 22",
+		image: "img/libra.png",
+		text: "As their zodiac signs and meanings would indicate, Libra's are all about balance, justice, equanimity and stability. They easily surround themselves with harmony and beauty, but sometimes go to extremes to do so if their goals are unreasonable or unhealthy. With Venus as their ruling planet, Libra's are very understanding, caring, and often the champion of underdogs. They have keen intuitions, but often don't give themselves enough credit for their perceptions. They can be quiet and shy if not persuaded to come out of their shell. Ironically and in spite of their introverted nature they make excellent debaters, often proving a point from out of seemingly nowhere."
+	},
+	{
+		name: "scorpio",
+		date: "October 23 - November 21",
+		image: "img/scorpio.png",
+		text: "The Scorpio is often misunderstood. These personalities are bold and are capable of executing massive enterprises with cool control and confidence. They can surmount seemingly all obstacles when they put their mind to the task, and they have unshakable focus when the situation calls for it. Regardless of their bold nature, they are often secretive, but they are always observing behind their withdrawn manner."
+	},
+	{
+		name: "sagittarius",
+		date: "November 22 - December 21",
+		image: "img/sagittarius.png",
+		text: "Here we have the philosopher among the zodiac signs and meanings. Like the Scorpio, they have great ability for focus, and can be very intense. However, they must channel their energy or they will waste time and wear themselves out going in too many directions at once. They are not very patient and expect quick results. However, when encountered with failure they make extreme comeback's often against incredible odds. They make loyal friends and lovers, but they do not handle commitment well as they refuse to be tied down while chasing philosophical pursuits."
+	},
+	{
+		name: "capricorn",
+		date: "December 22 - January 19",
+		image: "img/capricorn.png",
+		text: "Capricorn's are also philosophical signs and are highly intelligent too. They apply their knowledge to practical matters, and strive to maintain stability and order. They are good organizers, and they achieve their goals by purposeful, systematic means. They are very intuitive, although they don't share this trait with others freely. They do not deal well with opposition or criticism but a healthy Capricorn will often shrug off negative comments towards their character. They are patient and persevering - they know they can accomplish any task as long as they follow a their plan step-by-step. Capricorn's have broad shoulders, and typically take on other's problems with aplomb. Ironically, they rarely share their own problems and tend to go through bouts of inner gloom after a spell of dwelling on these problems."
+	},
+	{
+		name: "aquarius",
+		date: "January 20 - February 18",
+		image: "img/aquarius.png",
+		text: "Often simple and unassuming, the Aquarian goes about accomplishing goals in quiet, often unorthodox ways. Although their methods may be unorthodox, the results for achievement are surprisingly effective. Aquarian's will take up any cause, and are humanitarians of the zodiac. They are honest, loyal and highly intelligent. They are also easy going and make natural friendships. If not kept in check, the Aquarian can be prone to sloth and laziness. However, they know this about themselves, and try their best to motivate themselves to action. They are also prone to philosophical thoughts, and are often quite artistic and poetic."
+	},
+	{
+		name: "pisces",
+		date: "February 19 - March 20",
+		image: "img/pisces.png",
+		text: "Also unassuming, the Pisces zodiac signs and meanings deal with acquiring vast amounts of knowledge, but you would never know it. They keep an extremely low profile compared to others in the zodiac. They are honest, unselfish, trustworthy and often have quiet dispositions. They can be overcautious and sometimes gullible. These qualities can cause the Pisces to be taken advantage of, which is unfortunate as this sign is beautifully gentle, and generous. In the end, however, the Pisces is often the victor of ill circumstance because of his/her intense determination. They become passionately devoted to a cause - particularly if they are championing for friends or family."
+	},
+]
+function getInfo() {
+	var input = document.getElementById("input").value.toLowerCase();
+	// var button = document.getElementById("button");
+	var horoscopename = document.getElementById("horoscopename");
+	var picture = document.getElementById("picture");
+	var text = document.getElementById("description")
+	console.log(input)
+	for (var i=0; i < horoscope.length; i++) {
+		if (input == horoscope[i].name) {
+			// horoscopename.textContent = horoscope[i].name;
+			date.textContent = horoscope[i].date;
+			picture.src = horoscope[i].image;
+			description.textContent = horoscope[i].text;
+		}
+		// } else {
+		// 	console.log("Oops Horoscope does not exist!");
+		// }
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
